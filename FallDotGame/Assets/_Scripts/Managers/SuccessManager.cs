15f935f1e0e5a87a21f65832750a14768165de0b
EliseGabilly <elise.gabilly@gmail.com>
2022-07-09 20:31:56 +0200

    Succes refont first draft

--- FallDotGame/Assets/_Scripts/Managers/SuccessManager.cs
+++ FallDotGame/Assets/_Scripts/Managers/SuccessManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,14 +7,51 @@ public class SuccessManager : Singleton<SuccessManager> {
 
     #region Variables
     [SerializeField]
-    private Text scores;
+    private Text score;
+    [SerializeField]
+    private Text bonus;
+    [SerializeField]
+    private Text distance;
 
+    [SerializeField]
+    private Text death;
+    [SerializeField]
+    private Text clock;
+    [SerializeField]
+    private Text power_up;
+    [SerializeField]
+    private Text lines;
+
+    private Text[] txtBoxes;
     #endregion
 
     public void LoadSuccess() {
-        scores.text = string.Format("High score : {0}\nBy distance : {1}\nFrom bonus : {2}", 
-            Player.Instance.highScore, Player.Instance.highBonusScore, Player.Instance.highDistanceScore);
+        score.text = string.Format("Last score\n{0}", Player.Instance.highScore.ToString());
+        bonus.text = string.Format("{0}\nBonus", Player.Instance.highBonusScore.ToString());
+        distance.text = string.Format("{0}\nDistance", Player.Instance.highDistanceScore.ToString());
+
+        death.text = string.Format("Death reason : {0}", Player.Instance.highDistanceScore.ToString());
+        clock.text = string.Format("Time played : {0}", Player.Instance.highDistanceScore.ToString());
+        power_up.text = string.Format("Pick up : {0}", Player.Instance.highDistanceScore.ToString());
+        lines.text = string.Format("Line drawns : {0}", Player.Instance.highDistanceScore.ToString());
+
+        txtBoxes = new Text[] {bonus, distance, death, clock, power_up, lines};
+        StartCoroutine(SetTextSize());
     }
 
+    private IEnumerator SetTextSize() {
+        yield return null;
+
+        int minSize = 1000;
+
+        for (int i = 0; i < txtBoxes.Length; i++) {
+            if (minSize > txtBoxes[i].cachedTextGenerator.fontSizeUsedForBestFit)
+                minSize = txtBoxes[i].cachedTextGenerator.fontSizeUsedForBestFit;
+        }
+
+        for (int i = 0; i < txtBoxes.Length; i++) {
+            txtBoxes[i].resizeTextMaxSize = minSize;
+        }
+    }
 
 }
