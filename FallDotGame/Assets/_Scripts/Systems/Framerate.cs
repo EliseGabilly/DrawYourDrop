79d8fa10a6bf07c999272254a3708a0507d5a3eb
EliseGabilly <elise.gabilly@gmail.com>
2022-07-24 10:41:30 +0200

    Improve framerate

---
+++ FallDotGame/Assets/_Scripts/Systems/Framerate.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+ public class Framerate : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Application.targetFrameRate = 80;
+    }
+}
---
+++ FallDotGame/Assets/_Scripts/TestFps.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestFps : MonoBehaviour {
+
+    private int FramesPerSec;
+    private float frequency = 1.0f;
+    [SerializeField]
+    private Text fps;
+
+    void Start() {
+        StartCoroutine(FPS());
+    }
+
+    private IEnumerator FPS() {
+        for (; ; ) {
+            // Capture frame-per-second
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds(frequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            // Display it
+
+            fps.text = string.Format("FPS: {0}", Mathf.RoundToInt(frameCount / timeSpan));
+        }
+    }
+}
