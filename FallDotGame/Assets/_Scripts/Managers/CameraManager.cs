ead03ba3edb8601d787b983e1f4bf70c7dc98110
EliseGabilly <elise.gabilly@gmail.com>
2024-03-30 18:47:00 +0100

    Add acceleration

--- FallDotGame/Assets/_Scripts/Managers/CameraManager.cs
+++ FallDotGame/Assets/_Scripts/Managers/CameraManager.cs
@@ -5,8 +5,6 @@ public class CameraManager : MonoBehaviour {
     #region Variables
     private Camera mainCamera;
     private Transform playerTransform; 
-    [SerializeField]
-    private float movingSpeed = 1.2f;
     #endregion
 
     private void Awake() {
@@ -16,12 +14,17 @@ public class CameraManager : MonoBehaviour {
 
     private void Update() {
         if (GameManager.Instance.UseGravity) {
+            // find player position 
             Vector3 v3 = mainCamera.transform.position;
             v3.y = playerTransform.position.y + 2.4f; //TODO find magic value
             Vector3 playerPos = v3;
+
+            // find constant movement
+            float acceleration = GameManager.Instance.DistanceScore > 30 ? GameManager.Instance.DistanceScore - 30 : 0;
+            float movingSpeed = Mathf.Min(6 + acceleration / 25, 10);
             Vector3 constMovePos = mainCamera.transform.position + Vector3.down * Time.deltaTime * movingSpeed;
 
-            //Camera match either a constante movement or the player position if he is faster
+            //Camera match either the constant movement or the player position if he is faster
             Vector3 lowestPos = playerPos.y < constMovePos.y ? playerPos : constMovePos;
             mainCamera.transform.position = lowestPos;
         }
--- FallDotGame/Assets/_Scripts/Managers/GameManager.cs
+++ FallDotGame/Assets/_Scripts/Managers/GameManager.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager> {
 
